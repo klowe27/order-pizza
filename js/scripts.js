2ab60913fb804ad4468dd8b97eab5bf450dca761
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-11-02 09:37:47 -0700

    Add order constructor and basic UI

--- js/scripts.js
+++ js/scripts.js
@@ -16,29 +16,46 @@ function Pizza(size, crust, toppings, extras) {
 }
 
 Pizza.prototype.calculateCost = function() {
-  if (this.size = "small") {
+  if (this.size === "small") {
     this.cost += 8;
-  } else if (this.size = "medium") {
+  } else if (this.size === "medium") {
     this.cost += 10;
-  } else if (this.size = "large") {
+  } else if (this.size === "large") {
     this.cost += 12;
-  } else if (this.size = "x-large") {
-    this.cost += 14;
   }
   for (var i = 0; i < this.toppings.length - 2; i++) {
-    this.cost += .50;
+    this.cost += .5;
   }
   for (var i = 0; i < this.extras.length; i++) {
-    this.cost += 1.00;
+    this.cost += 1;
   }
   return this.cost;
 }
 
-var pizza = new Pizza ("medium", "thin", ["peperoni", "sausage", "pepper", "onion"], ["cheese"]);
-
-pizza.calculateCost();
-
 // User Interface Logic
 $(document).ready(function(){
+  var order = new Order();
+  $("#pizzaForm").submit(function(event){
+    event.preventDefault();
+    var size = $("input:radio[name=size]:checked").val();
+    var crust = $("input:radio[name=crust]:checked").val();
+    var toppings = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var topping = $(this).val();
+      toppings.push(topping);
+    });
+    var extras = [];
+    $("input:checkbox[name=extras]:checked").each(function(){
+      var extra = $(this).val();
+      extras.push(extra);
+    });
+    var pizza = new Pizza (size, crust, toppings, extras);
+    order.addPizza(pizza);
+    var cost = pizza.calculateCost();
+    console.log(size);
+    console.log(pizza);
+    console.log(order);
+    console.log(cost);
 
+  });
 });
